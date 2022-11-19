e95ab72c6d358fbfa7ab51bc03ca2756cc65a451
HornsGuy <37481699+HornsGuy@users.noreply.github.com>
2022-11-19 09:35:15 -0700

    Add project files.

---
+++ ServerPatches/HarmonyHelper.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerPatches
+{
+    public static class HarmonyHelper
+    {
+        public static void AddPrefix(Harmony harmony, Type classToPatch, string functionToPatchName, BindingFlags flags, Type patchClass, string functionPatchName)
+        {
+            var functionToPatch = classToPatch.GetMethod(functionToPatchName, flags);
+            var newHarmonyPatch = patchClass.GetMethod(functionPatchName);
+            harmony.Patch(functionToPatch, prefix: new HarmonyMethod(newHarmonyPatch));
+        }
+    }
+}
---
+++ ServerPatches/ServerPatches.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade;
+using ServerPatches.Patches;
+
+namespace ServerPatches
+{
+    public class ServerPatches : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Setup();
+        }
+
+        private void Setup()
+        {
+            Logging.Instance.StartLogging("ServerPatches", Logging.LogLevel.Info);
+
+            Harmony harmony = new Harmony("ServerPatches");
+
+            HarmonyHelper.AddPrefix(harmony, typeof(MissionLobbyComponent), "SendPeerInformationsToPeer", BindingFlags.NonPublic | BindingFlags.Instance, typeof(PatchMissionLobbyComponent_SendPeerInformationsToPeer), "Prefix");
+            HarmonyHelper.AddPrefix(harmony, typeof(MissionNetworkComponent), "SendSpawnedMissionObjectsToPeer", BindingFlags.NonPublic | BindingFlags.Instance, typeof(PatchMissionNetworkComponent), "Prefix");
+        }
+
+    }
+}
