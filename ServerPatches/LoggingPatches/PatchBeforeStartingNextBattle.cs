6284dc5cdfb5e31f7940d883484365eace5a7b56
HornsGuy <37481699+HornsGuy@users.noreply.github.com>
2022-11-23 10:45:04 -0700

    Reworked logging to allow for many different log instances, Patch an issue with REST messages to master server, and Added a Settings class

---
+++ ServerPatches/LoggingPatches/PatchBeforeStartingNextBattle.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade.Diamond;
+
+namespace ServerPatches.LoggingPatches
+{
+    [HarmonyPatch(typeof(CustomBattleServer), "BeforeStartingNextBattle")]
+    public class PatchBeforeStartingNextBattle
+    {
+        //TODO: We still need to clean out the gameLogs array somehow. That array is going to continue growing forever which is a problem
+        static int count = 1;
+        public static bool Prefix(CustomBattleServer __instance, GameLog[] gameLogs)
+        {
+            if(Settings.AreStoringAddGameLogsMessagesToLogFile())
+            {
+                Logging.Rest.Info($"Begin AddGameLogsMessage Override {count}: ");
+                foreach (var log in gameLogs)
+                {
+                    Logging.Rest.Info($"{log.Type} {log.Player} {log.GameTime} {log.GetDataAsString()}");
+                }
+
+                Logging.Rest.Info($"End AddGameLogsMessage Override {count}");
+
+                count += 1;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ServerPatches/Settings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+namespace ServerPatches
+{
+    class SettingsInstance
+    {
+        public bool LoggingAddGameLogsMessages = false;
+        public int NumberOfLogsToKeep = 10;
+        public int NumberOfRestLogsToKeep = 3;
+        public string SettingsVersion = "1.0.0";
+    }
+
+    public class Settings
+    {
+
+        private static SettingsInstance _instance;
+        
+
+        public static void LoadSettings(string jsonPath)
+        {
+            Directory.CreateDirectory("ServerPatches");
+
+            if(File.Exists(jsonPath))
+            {
+                SettingsInstance readIn = JsonConvert.DeserializeObject<SettingsInstance>(File.ReadAllText(jsonPath));
+                if(readIn != null )
+                {
+                    _instance = readIn;
+                }
+            }
+            else
+            {
+                _instance = new SettingsInstance();
+
+                // Settings doesn't exist, save
+                SaveSettings(jsonPath);
+            }
+        }
+
+        public static void SaveSettings(string jsonPath)
+        {
+            File.WriteAllText(jsonPath,JsonConvert.SerializeObject(_instance,Formatting.Indented));
+        }
+
+        public static int GetNumberOfLogsToKeep()
+        {
+            return _instance.NumberOfLogsToKeep;
+        }
+        public static int GetNumberOfRestLogsToKeep()
+        {
+            return _instance.NumberOfRestLogsToKeep;
+        }
+
+        public static bool AreStoringAddGameLogsMessagesToLogFile()
+        {
+            return _instance.LoggingAddGameLogsMessages;
+        }
+
+    }
+}
